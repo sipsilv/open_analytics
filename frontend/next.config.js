52c0ca747d2e37b8f42026f52913cd5233835d98
Santhosh Jallu <santhoshjallu@gmail.com>
2026-01-18 12:15:42 +0530

    ui changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  }
+  output: 'standalone',
+  swcMinify: true,
+  compress: true,
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Disable TypeScript errors during production builds
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
-
